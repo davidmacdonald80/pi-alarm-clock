9d00433ad622064a8a166f71fb7cefd68a4f8047
davidmacdonald80 <dugle80@gmail.com>
2024-05-11 18:20:55 -0500

    prep for pytest

--- update_requirements.sh
+++ update_requirements.sh
@@ -2,7 +2,7 @@
 source /home/david/alarm/bin/activate
 echo "Running pipreqs from script" >> /home/david/alarm/script.log
 # Generate requirements.txt for Python project
-cd /home/david/alarm
+cd /home/david/alarm/src
 /home/david/alarm/bin/pipreqs . --ignore bin,lib,include,share,__pycache__ --force
 echo "Script completed" >> /home/david/alarm/script.log
 deactivate
